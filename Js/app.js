c2b7eafd1e9ee826dbfc15ca540a218ec3c84b47
Ignatius22 <igee54077@gmail.com>
2020-09-14 14:54:24 +0100

    add player factory

---
+++ Js/app.js
@@ -0,0 +1,12 @@
+const playerFactory = (name, mark) => {
+  const playTurn = (board, cell) => {
+    const idx = board.cells.findIndex(position => position === cell);
+    if (board.boardArray[idx] === '') {
+      board.render();
+      return idx;
+    }
+    return null;
+  };
+
+  return { name, mark, playTurn };
+};
