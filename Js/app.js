abfd7333a9db25f5089827dd27c3323f137be212
Ignatius22 <igee54077@gmail.com>
2020-09-14 15:11:38 +0100

    add conditions for displaying gameboard and current player

--- Js/app.js
+++ Js/app.js
@@ -62,23 +62,43 @@ const boardModule = (() => {
 
 
 const gamePlay = (() => {
-	  const playerOneName = document.querySelector('#player1');
-  const playerTwoName = document.querySelector('#player2');
-  const form = document.querySelector('.player-info');
-  const resetBtn = document.querySelector('#reset');
-  let currentPlayer;
-  let playerOne;
-  let playerTwo;
+    const playerOneName = document.querySelector('#player1');
+    const playerTwoName = document.querySelector('#player2');
+    const form = document.querySelector('.player-info');
+    const resetBtn = document.querySelector('#reset');
+    let currentPlayer;
+    let playerOne;
+    let playerTwo;
 
     const switchTurn = () => {
-    currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
-  };
+        currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+    };
     const gameRound = () => {
-    const board = boardModule;
-    const gameStatus = document.querySelector('.game-status');
-    if (currentPlayer.name !== '') {
-      gameStatus.textContent = `${currentPlayer.name}'s Turn`;
-    } else {
-      gameStatus.textContent = 'Board: ';
-    }
+        const board = boardModule;
+        const gameStatus = document.querySelector('.game-status');
+        if (currentPlayer.name !== '') {
+            gameStatus.textContent = `${currentPlayer.name}'s Turn`;
+        } else {
+            gameStatus.textContent = 'Board: ';
+        }
+        board.gameBoard.addEventListener('click', (event) => {
+            event.preventDefault();
+            const play = currentPlayer.playTurn(board, event.target);
+            if (play !== null) {
+                board.boardArray[play] = `${currentPlayer.mark}`;
+                board.render();
+                const winStatus = board.checkWin();
+                if (winStatus === 'Tie') {
+                    gameStatus.textContent = 'Tie!';
+                } else if (winStatus === null) {
+                    switchTurn();
+                    gameStatus.textContent = `${currentPlayer.name}'s Turn`;
+                } else {
+                    gameStatus.textContent = `Winner is ${currentPlayer.name}`;
+                    board.reset();
+                    board.render();
+                }
+            }
+        });
+    };
 })();
